02c73c0947df06a6e4143fc908d7bc2e20cf2389
Anthony <anthony_bouillon@outlook.fr>
2017-11-02 11:27:13 +0100

    correction des functions

--- questions.js
+++ questions.js
@@ -1,8 +1,7 @@
 // Créer un tableau "langages" contenant "Html", "CSS", "Java", "PHP"
 var CreationTableauLangages = function () {
-    //CreationTableauLangages = array('Html', 'Css', 'Java', 'PHP');
     CreationTableauLangages = ['Html', 'CSS', 'Java', 'PHP'];
-  return CreationTableauLangages;
+    return CreationTableauLangages;
 }
 // Créer un tableau "nombres" contenant les nombres de 0 à 5
 var CreationTableauNombres = function () {
@@ -11,51 +10,46 @@ var CreationTableauNombres = function () {
 }
 //Remplacer le troisième élément du tableau par "Javascript"
 var RemplacementElement = function (langages) {
-  RemplacementElement = ['Html', 'CSS', 'Java', 'PHP'];
-    RemplacementElement.splice(2, 1, 'Javascript');
-  return RemplacementElement;
+    langages[2] = 'Javascript';
+    return langages;
 }
 //Ajouter "Ruby" et "Python" à la fin du tableau
 var AjoutElementLangages = function (langages) {
-    AjoutElementLangages = ['Html', 'CSS', 'Javascript', 'PHP'];
-    AjoutElementLangages.push('Ruby', 'Python');
-    return AjoutElementLangages;
+    langages.push('Ruby', 'Python');
+    return langages;
 }
  //Ajouter "-2" et "-1" au début du tableau nombres
 var AjoutElementNombres = function (nombres) {
-    AjoutElementNombres = [0, 1, 2, 3, 4, 5 ];
-    AjoutElementNombres.unshift(-2, -1);
-  return AjoutElementNombres;
+    nombres.unshift(-2, -1);
+    return nombres;
 }
 //Supprimer le premier élément du tableau langages
 var SuppressionPremierElement = function (langages) {
-    SuppressionPremierElement = ['CSS', 'Javascript', 'PHP', 'Ruby', 'Python'];
-    SuppressionPremierElement.splice(0, 0);
-  return SuppressionPremierElement;
+    langages.shift();
+    return langages;
 }
 //Supprimer le dernier élément du tableau langages
 var SuppressionDernierElement = function (langages) {
-    SuppressionDernierElement = ['CSS', 'Javascript', 'PHP', 'Ruby', 'Python'];
-    SuppressionDernierElement.splice(4,1);
-  return SuppressionDernierElement;
+    langages.pop();
+    return langages;
 }
 //Faire de la chaîne "reseaux_sociaux_chaine" un tableau "reseaux_sociaux"
 var ConversionChaineTableau = function (réseaux_sociaux_chaine) {
-    ConversionChaineTableau = ['Facebook', 'Twitter', 'Google +', 'Viadeo', 'LinkedIn'];
-  return ConversionChaineTableau;
+    réseaux_sociaux = réseaux_sociaux_chaine.split(',');
+    return réseaux_sociaux;
 }
 //Faire du tableau "langages" une chaîne "langages_chaine". Séparer les langages par une virgule
 var ConversionTableauChaine = function (langages) {
-    ConversionTableauChaine= ('CSS,Javascript,PHP,Ruby');
-  return ConversionTableauChaine;
+    langages_chaine = langages.join(',');
+    return langages_chaine;
 }
 //Trier le tableau "reseaux_sociaux"
 var TriTableau = function (reseaux_sociaux) {
     reseaux_sociaux.sort();
-  return reseaux_sociaux;
+    return reseaux_sociaux;
 }
 //Inverser le tableau "langages"
 var InversionTableau = function (reseaux_sociaux){
     InversionTableau = reseaux_sociaux.reverse();
-  return InversionTableau;
+    return InversionTableau;
 }
